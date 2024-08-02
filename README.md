f4a3595aef594f4a9a10194c6e32bec09495c717
Montassir Kounsi <montassir.kounsi@se.com>
2024-08-02 15:53:20 +0200

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# dpdk_app
+# Description
+This tool polls ethernet interface and sends back received SV and Goose packets. It is used in RT network benchmarking to modelise P7V application's network processing.
+
+
+# Build
+Run meson commands for build build. Binary is created in build folder
+```
+meson build
+cd build
+ninja
+```
+
--- dpdk_loopback.c
+++ dpdk_loopback.c
@@ -20,6 +20,7 @@
 #define BURST_SIZE 32
 #define SV_ETHERTYPE 0x88BA
 #define GOOSE_ETHERTYPE 0x88B8
+#define APPID_OFFSET 14
 
 #define CYCLE_TIME_NS (300 * 1000)
 #define NSEC_PER_SEC (1000 * 1000 * 1000)
@@ -104,9 +105,6 @@ port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 			   " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
 			port, RTE_ETHER_ADDR_BYTES(&addr));
 
-	/* Enable RX in promiscuous mode for the Ethernet device. */
-	// retval = rte_eth_promiscuous_enable(port);
-	/* End of setting RX port in promiscuous mode. */
 	if (retval != 0)
 		return retval;
 
@@ -115,13 +113,10 @@ port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 /* >8 End of main functional part of port initialization. */
 
 /*
- * The lcore main. This is the main thread that does the work, reading from
- * an input port and writing to an output port.
+ * The loopback thread. It polls the ethernet port at a defined frequency, 
+ 	and sends back Goose and SV packets.
  */
-
- /* Basic forwarding application lcore. 8< */
-static __rte_noreturn void
-lcore_main(void)
+static __rte_noreturn void loopback_main(void)
 {
 	uint16_t port;
 	struct rte_ether_hdr *eth_hdr;
@@ -130,8 +125,7 @@ lcore_main(void)
 	struct rte_ether_addr src_mac;
 	struct rte_ether_addr dst_mac;
 	struct timespec tv;
-	uint8_t * data;
-	uint16_t appid;
+	uint8_t * appid;
 
 	/*
 	 * Check that the port is on the same NUMA node as the polling thread
@@ -151,104 +145,93 @@ lcore_main(void)
 	/* Main work of application loop. 8< */
 	clock_gettime(CLOCK_MONOTONIC, &tv);
 	for (;;) {
-		/*
-		 * Receive packets on a port and forward them on the paired
-		 * port. The mapping is 0 -> 1, 1 -> 0, 2 -> 3, 3 -> 2, etc.
-		 */
-		RTE_ETH_FOREACH_DEV(port) {
 
-			/* Get burst of RX packets, from first port of pair. */
-			struct rte_mbuf *bufs[BURST_SIZE];
-			const uint16_t nb_rx = rte_eth_rx_burst(port, 0,
-					bufs, BURST_SIZE);
+		/* Get burst of RX packets */
+		struct rte_mbuf *bufs[BURST_SIZE];
+		const uint16_t nb_rx = rte_eth_rx_burst(0, 0,
+				bufs, BURST_SIZE);
 
-			if (nb_rx) {
-				for (j = 0; j < nb_rx; j++) {
-					struct rte_mbuf *m = bufs[j];
+		if (nb_rx) {
+			for (j = 0; j < nb_rx; j++) {
+				struct rte_mbuf *m = bufs[j];
 
-					eth_hdr = rte_pktmbuf_mtod(m,
-							struct rte_ether_hdr *);
-					ethertype = eth_hdr->ether_type;
-					if(rte_cpu_to_be_16(SV_ETHERTYPE) == ethertype || rte_cpu_to_be_16(GOOSE_ETHERTYPE) == ethertype)
-					{
-						data = rte_pktmbuf_mtod_offset(m, uint8_t *, 14);
-						data[1] = data[0];
-						data[0] = 0; 
-						printf("SV/Goose received !\n");
-						rte_ether_addr_copy(&eth_hdr->src_addr, &dst_mac);
-						rte_ether_addr_copy(&eth_hdr->dst_addr, &src_mac);
-						rte_ether_addr_copy(&dst_mac, &eth_hdr->dst_addr);
-						rte_ether_addr_copy(&src_mac, &eth_hdr->src_addr);
-						const uint16_t nb_tx = rte_eth_tx_burst(port, 0,
-								&m, 1);
-						if (unlikely(!nb_tx)) {
-							rte_pktmbuf_free(m);
-						}
-					}
-					else
-					{
+				eth_hdr = rte_pktmbuf_mtod(m,
+						struct rte_ether_hdr *);
+				ethertype = eth_hdr->ether_type;
+				/* Loopback Goose and SV packets */
+				if(rte_cpu_to_be_16(SV_ETHERTYPE) == ethertype || rte_cpu_to_be_16(GOOSE_ETHERTYPE) == ethertype)
+				{
+					/* Flip SRC and DST MAC */
+					rte_ether_addr_copy(&eth_hdr->src_addr, &dst_mac);
+					rte_ether_addr_copy(&eth_hdr->dst_addr, &src_mac);
+					rte_ether_addr_copy(&dst_mac, &eth_hdr->dst_addr);
+					rte_ether_addr_copy(&src_mac, &eth_hdr->src_addr);
+					/* Set second APPID byte second as the first byte and set first byte as 0 */
+					/* APPID first byte is a counter reserved for sender, APPID second byte is a counter reserved for receiver */
+					appid = rte_pktmbuf_mtod_offset(m, uint8_t *, APPID_OFFSET);
+					appid[1] = appid[0];
+					appid[0] = 0; 
+					/* Send back packet */
+					const uint16_t nb_tx = rte_eth_tx_burst(0, 0,
+							&m, 1);
+					if (unlikely(!nb_tx)) {
 						rte_pktmbuf_free(m);
 					}
-					
+				}
+				else
+				{
+					rte_pktmbuf_free(m);
 				}
 			}
-			/* wait for next cycle */
-			tv.tv_nsec += CYCLE_TIME_NS;
-			norm_ts(&tv);
-			clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &tv, NULL);
 		}
+		/* wait for next cycle */
+		tv.tv_nsec += CYCLE_TIME_NS;
+		norm_ts(&tv);
+		clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &tv, NULL);
 	}
 	/* >8 End of loop. */
 }
-/* >8 End Basic forwarding application lcore. */
 
 /*
- * The main function, which does initialization and calls the per-lcore
- * functions.
+ * The main function, which does dpdk rte initializations.
  */
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	struct rte_mempool *mbuf_pool;
 	unsigned nb_ports;
-	uint16_t portid;
 
-	/* Initializion the Environment Abstraction Layer (EAL). 8< */
+	/* Initialization the Environment Abstraction Layer (EAL). 8< */
 	int ret = rte_eal_init(argc, argv);
 	if (ret < 0)
 		rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
 	/* >8 End of initialization the Environment Abstraction Layer (EAL). */
 
-	argc -= ret;
-	argv += ret;
-
-	/* Check that there is an even number of ports to send/receive on. */
+	/* Check that there is atleast a port to send/receive on. */
 	nb_ports = rte_eth_dev_count_avail();
-	// if (nb_ports < 2 || (nb_ports & 1))
-	// 	rte_exit(EXIT_FAILURE, "Error: number of ports must be even\n");
+	if (nb_ports < 1)
+		rte_exit(EXIT_FAILURE, "Error: no usable port found \n");
 
 	/* Creates a new mempool in memory to hold the mbufs. */
 
 	/* Allocates mempool to hold the mbufs. 8< */
-	mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
+	mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS,
 		MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
 	/* >8 End of allocating mempool to hold mbuf. */
 
 	if (mbuf_pool == NULL)
 		rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
 
-	/* Initializing all ports. 8< */
-	RTE_ETH_FOREACH_DEV(portid)
-		if (port_init(portid, mbuf_pool) != 0)
-			rte_exit(EXIT_FAILURE, "Cannot init port %"PRIu16 "\n",
-					portid);
-	/* >8 End of initializing all ports. */
+	/* Initializing first port. 8< */
+	if (port_init(0, mbuf_pool) != 0)
+		rte_exit(EXIT_FAILURE, "Cannot init port %"PRIu16 "\n",
+				0);
+	/* >8 End of initializing first port. */
 
 	if (rte_lcore_count() > 1)
 		printf("\nWARNING: Too many lcores enabled. Only 1 used.\n");
 
-	/* Call lcore_main on the main core only. Called on single lcore. 8< */
-	lcore_main();
+	/* Call loopback main thread on the main core only. Called on single lcore. 8< */
+	loopback_main();
 	/* >8 End of called on single lcore. */
 
 	/* clean up the EAL */
